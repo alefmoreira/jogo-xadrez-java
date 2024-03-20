d256777e7f668ebb4c3f0c753611a60a3f1fd968
alefmoreira <alefgouveia17@gmail.com>
2024-03-20 16:26:54 -0300

    melhoria na impressão do tabuleiro

--- src/chess/Partida.java
+++ src/chess/Partida.java
@@ -27,10 +27,19 @@ public class Partida {
         tabuleiro.lugarPeca(pecaXadrez, new XadrezPosicao(coluna, linha).toPositon());
     }
     private void configInicial(){
-        lugarNovaPeca('b', 5, new Torre(tabuleiro, Cor.BRANCO));
+        lugarNovaPeca('c', 1, new Torre(tabuleiro, Cor.BRANCO));
+        lugarNovaPeca('c', 2, new Torre(tabuleiro, Cor.BRANCO));
+        lugarNovaPeca('d', 2, new Torre(tabuleiro, Cor.BRANCO));
+        lugarNovaPeca('e', 2, new Torre(tabuleiro, Cor.BRANCO));
+        lugarNovaPeca('e', 1, new Torre(tabuleiro, Cor.BRANCO));
+        lugarNovaPeca('d', 1, new Rei(tabuleiro, Cor.BRANCO));
 
+        lugarNovaPeca('c', 7, new Torre(tabuleiro, Cor.PRETO));
+        lugarNovaPeca('c', 8, new Torre(tabuleiro, Cor.PRETO));
+        lugarNovaPeca('d', 7, new Torre(tabuleiro, Cor.PRETO));
+        lugarNovaPeca('e', 7, new Torre(tabuleiro, Cor.PRETO));
+        lugarNovaPeca('e', 8, new Torre(tabuleiro, Cor.PRETO));
+        lugarNovaPeca('d', 8, new Rei(tabuleiro, Cor.PRETO));
     }
 
-
-
-}
+    }
